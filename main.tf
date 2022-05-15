40b9ee3ed8f9fb58d2330d77ede1d9be1dd88056
Nico TSESSILEDE <31391553+nicotsessilede@users.noreply.github.com>
2022-05-15 03:33:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "example" {
